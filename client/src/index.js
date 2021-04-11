77fb768c6ede14281ac4ee4312c3b05dc253f27d
Xiaoxi <yxx0827@gmail.com>
2021-04-10 21:40:23 -0400

    add polyfills

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,11 @@
 // Polyfills
+import 'core-js/es/map';
+import 'core-js/es/set';
+import 'raf/polyfill';
+import 'core-js/features/array/find';
+import 'core-js/features/array/includes';
+import 'core-js/features/number/is-nan';
+
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
