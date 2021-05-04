3329a4c64cfe60cb55f8fcf06eb337d2586bbe52
Xiaoxi <yxx0827@gmail.com>
2021-05-04 17:02:02 -0400

    update pm2 config

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  name: 'nest-server',
-  script: 'dist/main.js',
-  watch: false,
-  restart_delay: 5000
+  apps: [{
+    name: 'nest-server',
+    script: 'dist/main.js',
+    watch: false,
+    restart_delay: 5000,
+    error_file: './logs/error.log',
+    out_file: './logs/out.log'
+  }]
 };
