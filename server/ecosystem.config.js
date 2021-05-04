ffd43252acb2c915a69272978d3b22c129e395ba
Xiaoxi <yxx0827@gmail.com>
2021-05-04 17:36:42 -0400

    update pm2 config

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     script: 'dist/main.js',
     watch: false,
     restart_delay: 5000,
-    error_file: './logs/error.log',
-    out_file: './logs/out.log'
+    error_file: '/var/log/pm2/error.log',
+    out_file: '/var/log/pm2/out.log'
   }]
 };
